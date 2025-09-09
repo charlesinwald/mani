5a208761723628df06482433fd25c7f18a55b2e9
Charles Inwald <cinwald@gmail.com>
2025-09-09 16:43:11 -0400

    Integrate react-native-splash-screen into the project, updating package dependencies and Android configuration. Removed react-native-bootsplash and added necessary imports and styles for the splash screen.

--- android/app/src/main/java/com/saheeranas/diaryapp/MainActivity.kt
+++ android/app/src/main/java/com/saheeranas/diaryapp/MainActivity.kt
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen // Import the SplashScreen package
+import com.zoontek.rnbootsplash.RNBootSplash
+import android.os.Bundle
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +22,10 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      RNBootSplash.init(R.drawable.bootsplash, this) // <- Initialize bootsplash
+
+  }
 }
